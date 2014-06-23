f745825d73ec98a224faa7f4305109c472932cd7
iamleohandev <iamleohandev@gmail.com>
2014-06-22 21:10:01 -0400

    commit for the Spring JMS receiver
    1) setup the Spring configuration for the receiver
    
    jmsTemplate.receive()  the message store in the queue

--- src/main/java/com/leo/han/App.java
+++ src/main/java/com/leo/han/App.java
@@ -1,8 +1,13 @@
 package com.leo.han;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.jms.JMSException;
+import javax.jms.ObjectMessage;
 
-import com.leo.han.services.CalculateService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.leo.han.beans.User;
+
 
 /**
  * Hello world!
@@ -10,19 +15,22 @@ import com.leo.han.services.CalculateService;
  */
 public class App 
 {
-    public static void main( String[] args )
+
+    public static void main( String[] args ) throws JMSException
     {
         System.out.println( "start" );
         
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:client-config.xml");
         
-        CalculateService service = context.getBean(CalculateService.class);
         
-        
-        System.out.println(service.getSummary());
-        
-        System.out.println(service.sum(20, 30)); 
-        System.out.println( "end" );
-        
+       JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+       
+       ObjectMessage msg = (ObjectMessage)jmsTemplate.receive();
+       
+       User user = (User) msg.getObject();
+       
+       System.out.println("User Name:  "+user.getUsername());
+       System.out.println("User Age:  "+user.getAge());
+       System.out.println("User Name:  "+user.getComment());
     }
 }
